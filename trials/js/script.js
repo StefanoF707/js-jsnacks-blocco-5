43d1ce4d50e48c899115c6fe493f25b650caf67d
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-01 16:55:25 +0100

    altre prove

--- trials/js/script.js
+++ trials/js/script.js
@@ -1,17 +1,51 @@
-// Snack Rifatto con ES6
+$(document).ready(
+    function() {
 
-//Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+        // SNACK
 
-let number1 = 3;
-let number2 = 13;
+        let number1 = 3;
+        let number2 = 13;
 
-const alphabet = ["a", "b", "z", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        const alphabet = ["a", "b", "z", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-alphabet[2] = "c";
-console.log(alphabet);
-var alphabet2 = lettersInArray(alphabet, number1, number2);
-console.log(alphabet2);
+        alphabet[2] = "c";
+        console.log(alphabet);
+        var alphabet2 = lettersInArray(alphabet, number1, number2);
+        console.log(alphabet2);
 
+        let word = `${alphabet[2]}${alphabet[8]}${alphabet[0]}${alphabet[14]}`;
+        console.log(`Parola concatenata: ${word}.`);
+
+        // /SNACK
+
+
+        const getRandomNumber = (min, max) => Math.floor((Math.random() * (max - min) + 1) + min);
+
+        let player1 = getRandomNumber(0, 5);
+        let player2 = getRandomNumber(0, 5);
+
+        var winner;
+        if (player1 == player2) {
+            winner = "Pareggio!";
+        } else if (player1 > player2) {
+            winner = "Giocatore 1!";
+        } else {
+            winner = "Giocatore 2!";
+        }
+
+        var innerContainer = `
+        <h2>Giocatore 1: ${player1}</h2>
+        <h2>Giocatore 2: ${player2}</h2>
+        <h2>Vincitore: ${winner}</h2>
+        `;
+
+        $(".container").html(innerContainer);
+
+    }
+);
+
+
+//----------------------------------------------------
 
 function lettersInArray  (array, num1, num2) {
     let newArray = [];
@@ -20,6 +54,3 @@ function lettersInArray  (array, num1, num2) {
     }
     return newArray
 }
-
-let word = `${alphabet[2]}${alphabet[8]}${alphabet[0]}${alphabet[14]}`;
-console.log(`Parola concatenata: ${word}.`);
